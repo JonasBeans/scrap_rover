2383c8b014a8dccffc5c106bc0d2626635a171f3
Jonas Boon <boon.jonas.git@outlook.com>
2026-04-07 23:26:45 +0200

    Change folder names of that are used in start and stop script

--- stop.sh
+++ stop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for pidfile in ~/run/server.pid ~/run/mediamtx.pid; do
+for pidfile in ~/.run/server.pid ~/.run/mediamtx.pid; do
     if [ -f "$pidfile" ]; then
         kill $(cat "$pidfile") && echo "Stopped $(basename $pidfile .pid)"
         rm "$pidfile"
